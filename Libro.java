fc17326209a026b9958fb569d0f259f91e2d11ab
beatranche <56152901+beatranche@users.noreply.github.com>
2020-11-09 12:05:18 +0100

    1847.03 Se añade el número de páginas al constructor y un método que devuelve el número de las mismas

--- Libro.java
+++ Libro.java
@@ -11,14 +11,16 @@ public class Libro {
 
     private String autor;
     private String titulo;
+    private int numeroPaginas;
 
     /**
      * Fija el autor y el titulo del libro a los dados como parametro
      */
-    public Libro(String autorLibro, String tituloLibro)
+    public Libro(String autorLibro, String tituloLibro, int paginas)
     {
         autor = autorLibro;
         titulo = tituloLibro;
+        numeroPaginas = paginas;
     }
    
     /**
@@ -26,6 +28,13 @@ public class Libro {
      */
     public String getAutor(){
         return autor;
+    }
+    
+    /**
+     * Devuelve el numero de paginas del libro
+     */
+    public int getNumeroPaginas(){
+        return numeroPaginas;
     }
     
     /**
